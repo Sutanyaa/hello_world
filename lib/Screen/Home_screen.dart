88e597b7d1c871e17d37ccdedab75858553c0c6c
Sutanyaa <e61181050@gmail.com>
2021-03-07 13:34:06 +0700

    up

--- lib/Screen/Home_screen.dart
+++ lib/Screen/Home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/screen/FormScreen/Example.dart';
+import 'package:hello_world/screen/FormScreen/form_screen.dart';
+import 'package:hello_world/screen/list_view_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -13,31 +16,90 @@ class _HomeScreenState extends State<HomeScreen> {
     return Container(
       child: Scaffold(
         drawer: Drawer(
-          child: Text("Hello ... "),
+          child: Text("Hello Drawer"),
         ),
         appBar: AppBar(
-          title: Text("My First Project"),
+          title: Text(
+            "Home Screen",
+          ),
         ),
         body: Center(
           child: Column(
             children: [
+              Container(color: Colors.red, child: Text("flwkrjgwiu;N;S")),
+              Text("flwkrjgwiu;N;S"),
               Container(
+                width: 150,
+                height: 150,
                 color: Colors.red,
-                child: Text("asdf;laksdfj;laskdfj"))
-              Text("asdf;laksdfj;laskdfj"),
-              Container(
-                  height: 250,
-                  width: 250,
-                  color: Colors.deepOrange[100],
-                  child: Image.asset('assets/images/55.jpg')),
+                child: Image.asset('assets/images/55.jpg'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ListViewScreen(),
+                    ),
+                  );
+                },
+                child: Text("กดปุ่มนี้ดู"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FormScreen(),
+                    ),
+                  );
+                },
+                child: Text("FormScreen"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Example(),
+                    ),
+                  );
+                },
+                child: Text("Example"),
+              ),
+              DropdownButton(
+                hint: Text("น่ารักมั้ย"),
+                items: [
+                  DropdownMenuItem(
+                    onTap: () {},
+                    value: 1,
+                    child: Text("น่ารัก"),
+                  ),
+                  DropdownMenuItem(
+                    onTap: () {},
+                    value: 1,
+                    child: Text("ไม่น่ารัก"),
+                  )
+                ],
+                onChanged: (int value) {},
+              ),
             ],
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-            BottomNavigationBarItem(icon: Icon(Icons.build), label: "build"),
-            BottomNavigationBarItem(icon: Icon(Icons.phone), label: "phone"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: "Phone",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_customize),
+              label: "Dashboard",
+            ),
           ],
         ),
       ),
